2cddca4d9acd682cbf9f797de5dc4a54bc6786bb
Nit03uppal <125196793+Nit03uppal@users.noreply.github.com>
2024-10-22 16:00:22 +0530

    music_library.py
    Music library to play music

---
+++ music_library.py
@@ -0,0 +1,6 @@
+music={
+    "8 letters":"https://www.youtube.com/watch?v=C3DlM19x4RQ",
+    "7 stories":"https://www.youtube.com/watch?v=clKaWCk14GE",
+    "what am i":"https://www.youtube.com/watch?v=ZxgbQz4n8-s",
+    "night changes":"https://www.youtube.com/watch?v=syFZfO_wfMQ"
+}
